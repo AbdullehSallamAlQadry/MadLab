c05308affeb966588a2f3c23395117d405b9e2aa
AbdullehSallamAlQadry <abdullahsallamalqadry@gmail.com>
2025-12-12 01:01:44 +0300

    i build a popup for login and signup, and know and put an ex for svg icons for diagnostics page

---
+++ src/components/login.js
@@ -0,0 +1,29 @@
+'use client'
+import Popup from "reactjs-popup"
+import 'reactjs-popup/dist/index.css'
+
+export default function PopupLogin({btnLinkStyle, btnStyle}) {
+  return (
+      <Popup trigger={
+        <button className={`${btnLinkStyle} ${btnStyle}`}>Login</button>
+        } position="right center" modal nested>
+        {close => (
+          <div className="modal">
+            <button className="close" onClick={close}>
+              &times;
+            </button>
+            <div className="header"> GeeksforGeeks </div>
+            <div className="content">
+              This is a simple popup example.
+            </div>
+            <div className="actions">
+              <button className="button" onClick={() => {
+                console.log('Button clicked');
+                close();
+              }}>Click here</button>
+            </div>
+          </div>
+        )}
+      </Popup>
+  )
+}
